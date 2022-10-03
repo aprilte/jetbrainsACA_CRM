9f3efc30f162cd47073d0703d04d14762d68f580
aprilte <apriltexs@gmail.com>
2022-10-04 02:03:00 +0900

    bookMyCinema

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -2,27 +2,31 @@ package cinema
 
 fun main() {
     // write your code here
-    var MyCinema = MiniCinema()
-    //MyCinema.printSeatTable()
-    MyCinema.printTotalIncome()
+    println("Enter the number of rows:")
+    val row = readln().toInt()
+    println("Enter the number of seats in each row:")
+    val col = readln().toInt()
+
+    var MyCinema = MiniCinema(row, col)
+    MyCinema.printSeatTable()
+//    MyCinema.printTotalIncome()
+    MyCinema.book()
+    MyCinema.printSeatTable()
 }
 
-class MiniCinema {
-    var row: Int = 0
-    var col: Int = 0
+class MiniCinema (val row: Int, val col: Int) {
     var totalSeats: Int = 0
     var frontHalf: Int = 0
     var backHalf: Int = 0
     val boundOfLarge: Int = 60
+    var largeCinema: Boolean = false
+    val seatStat = Array(row, { Array(col, {0}) }).toMutableList()
 
     init {
-        println("Enter the number of rows:")
-        row = readln().toInt()
-        println("Enter the number of seats in each row:")
-        col = readln().toInt()
         totalSeats = row * col
         frontHalf = ( row / 2 ) * col
         backHalf = totalSeats - frontHalf
+        if(boundOfLarge < totalSeats) largeCinema = true
     }
 
     fun printTotalIncome() {
@@ -44,14 +48,48 @@ class MiniCinema {
         }
 
         //print each row
+        //issue::when col count is 10 or more, seat table style will be adjusted
         for (i in 1..this.row) {
             print("$i ")
             for (j in 1..this.col) {
-                print("S ")
+                if (seatStat[i-1][j-1] != 0) {
+                    print("B ")//booked
+                } else {
+                    print("S ")//seat available
+                }
                 if (j == this.col) println()
             }
         }
+    }
 
+    fun book() {
+        println("Enter a row number:")
+        val bRow = readln().toInt()
+        println("Enter a seat Number in that row:")
+        val bCol = readln().toInt()
+        //seat num check
+        if (this.row < bRow) {
+            println("out of seat table")
+            return
+        }
+        if (this.col < bCol) {
+            println("out of seat table")
+            return
+        }
+        //seat status update
+        seatStat[bRow-1][bCol-1] += 1
+        //price print
+        if (!largeCinema) {
+            println("Ticket price: $10")
+        } else {
+            println("Ticket price:" +
+                if (bRow <= this.row / 2) {
+                    "$10"
+                } else {
+                    "$8"
+                }
+           )
+        }
     }
 
 }
